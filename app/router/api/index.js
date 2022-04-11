a3cd29b773ca10714c74cfde07e1bf162c33c49e
Amirhossein Abazari <amirhossein.abazari@yahoo.com>
2022-04-11 15:07:53 +0430

    swagger and yaml added to index page

--- app/router/api/index.js
+++ app/router/api/index.js
@@ -2,8 +2,30 @@ const HomeController = require('../../http/controller/api/home.controller');
 
 const router = require('express').Router();
 
-router.post("/", HomeController.indexPage);
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: index of routes 
+ *      tags: [IndexPage]
+ *      description : get all need data for index page
+ *      parameters:
+ *          -   in: header
+ *              name: access-token
+ *              example: Bearer YourToken...
+ *      responses:
+ *          200:
+ *              description: success
+ *              schema: 
+ *                  type: string
+ *                  example : Index Page Store
+ *          404: 
+ *              description: not Found
+ */
+
+
+router.get("/", HomeController.indexPage);
 
 module.exports = {
-    HomeRouter: router, 
+    HomeRouter: router,
 }
--- app/server.js
+++ app/server.js
@@ -4,21 +4,39 @@ const morgan = require('morgan');
 const path = require('path');
 const { AllRoutes } = require('./router/router');
 const createError = require('http-errors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 class Application {
     #app = express();
     constructor(PORT, DB_URI) {
-        this.#app.use(morgan("dev"))
-        this.configApplication();
+        this.configApplication(PORT);
         this.connectToMongoDB(DB_URI);
         this.createServer(PORT);
         this.createRoutes();
         this.errorHandling();
+
     }
-    configApplication() {
+    configApplication(PORT) {
+        this.#app.use(morgan("dev"))
         this.#app.use(express.json());
         this.#app.use(express.urlencoded({ extended: true }));
         this.#app.use(express.static(path.join(__dirname, "..", 'public')))
-
+        this.#app.use("/api-doc",swaggerUi.serve, swaggerUi.setup(swaggerJsDoc({
+            swaggerDefinition: {
+                info: {
+                    title: "Store Backend",
+                    version: "1.0.0",
+                    description: "Store Backend Used Express , Node.js , Hapi , ..."
+                },
+                servers: [
+                    {
+                        url: `http://localhost:${PORT}`,
+                    }
+                ]
+            },
+            apis: ["./app/router/*/*.js"]
+        })));
     }
     createServer(PORT) {
         const http = require('http');
