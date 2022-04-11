dad69cddd3ad98ddc6eef652276ad07d2b296ddb
Amirhossein Abazari <amirhossein.abazari@yahoo.com>
2022-04-11 12:21:07 +0430

    blog model added

--- app/models/blogs.js
+++ app/models/blogs.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-
+    author: { type: mongoose.Types.ObjectId, required: true, },
+    title: { type: String, required: true, },
+    text: { type: String, required: true, },
+    image: { type: String, required: true, },
+    tag: { type: [String], default: [] },
+    category: { type: mongoose.Types.ObjectId, required: true, },
+    comments: { type: [], default: [] },
+    like: { type: [mongoose.Types.ObjectId], default: [] },
+    dislike: { type: [mongoose.Types.ObjectId], default: [] },
+    bookmark: { type: [mongoose.Types.ObjectId], default: [] },
 })
 
 module.exports = {
-    BlogModel: mongoose.model("", Schema)
+    BlogModel: mongoose.model("blog", Schema)
 }
