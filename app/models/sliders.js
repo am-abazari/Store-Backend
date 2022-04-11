b47e3fd771da7f3152b2d3f993e2fb534f22f59c
Amirhossein Abazari <amirhossein.abazari@yahoo.com>
2022-04-11 12:36:16 +0430

    slider model added

--- app/models/sliders.js
+++ app/models/sliders.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-
+    title: { type: String },
+    text: { type: String },
+    image: { type: String, required: true },
 })
 
 module.exports = {
