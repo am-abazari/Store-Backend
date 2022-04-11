aa070cd80044c554d1f4e9bf4e98eb7ef668f1ae
Amirhossein Abazari <amirhossein.abazari@yahoo.com>
2022-04-11 12:44:16 +0430

    refactor and morgan pkg

--- app/models/users.js
+++ app/models/users.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    username: { type: String, required: true },
+    first_name: { type: String, },
+    last_name: { type: String, },
+    username: { type: String, required: true, lowercase: true, },
     phone: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, lowercase : true, },
     password: { type: String, required: true },
     otp: {
         type: Object, default: {
@@ -17,7 +17,7 @@ const Schema = new mongoose.Schema({
     discount: { type: Number, default: 0 },
     birthday: { type: String, },
     roles: { type: [String], default: [] },
-    
+
 })
 
 module.exports = {
