352055a14b794724c60a04fc78d7f3c2ead12920
Amirhossein Abazari <amirhossein.abazari@yahoo.com>
2022-04-17 13:09:03 +0430

    verifyaccesstoken configed

--- app/http/middlewares/verifyAccessToken.js
+++ app/http/middlewares/verifyAccessToken.js
@@ -7,15 +7,15 @@ function VerifyAccessToken(req, res, next) {
     const headers = req.headers;
     const [bearer, token] = headers?.["access-token"]?.split(" ") || [];
     if (token && bearer?.toLowerCase() === "bearer") {
-        JWT.decode(token, ACCESS_TOKEN_SECRET_KEY, async(error, payload) => {
+        JWT.verify(token, ACCESS_TOKEN_SECRET_KEY, async(error, payload) => {
             if (error) return next(createHttpError.Unauthorized("Please Login to Your Account !"))
             const { mobile } = payload || {};
             const user = await UserModel.findOne({ mobile }, { password: 0, otp: 0 })
-            if (!user) createHttpError.Unauthorized("Username Not Found #404")
+            if (!user) return next(createHttpError.Unauthorized("Username Not Found #404"))
             req.user = user;
             return next();
         });
-    } return next(createHttpError.Unauthorized("Please Login to Your Account !"))
+    } else return next(createHttpError.Unauthorized("Please Login to Your Account !"))
 
 }
 
