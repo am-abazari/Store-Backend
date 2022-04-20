95316fde35e36b9bf74cb003e06953c55c79a9c1
Amirhossein Abazari <amirhossein.abazari@yahoo.com>
2022-04-21 00:17:28 +0430

    spaces removed

--- app/utils/secret_key_generator.js
+++ app/utils/secret_key_generator.js
@@ -1,5 +1,4 @@
 const crypto = require('crypto');
-
 const key = crypto.randomBytes(32).toString("hex").toUpperCase();
 console.log(key);
 // 69AC5255C61C22ECCC5E79CD88AFA496774B7282839CEC9F2EFB958A3B1EC5AD
