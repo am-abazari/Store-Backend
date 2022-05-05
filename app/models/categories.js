c814d3b7d942614590565aed00cff52a77336e9a
Amirhossein Abazari <92695669+am-abazari@users.noreply.github.com>
2022-05-05 11:37:47 +0430

    Update categories.js

--- app/models/categories.js
+++ app/models/categories.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const Schema = new mongoose.Schema({
     title: { type: String, required : true},
 })
-
 module.exports = {
     CategoriesModel: mongoose.model("category", Schema)
 }
