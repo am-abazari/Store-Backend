35b9b721a633718ca03f5ff677db7b2305cbea06
Amirhossein Abazari <92695669+am-abazari@users.noreply.github.com>
2022-05-07 14:49:00 +0430

    Update secret_key_generator.js

--- app/utils/secret_key_generator.js
+++ app/utils/secret_key_generator.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const key = crypto.randomBytes(32).toString("hex").toUpperCase();
 console.log(key);
+
 // 69AC5255C61C22ECCC5E79CD88AFA496774B7282839CEC9F2EFB958A3B1EC5AD
 // D2FC16EC37A0F91E48AE2BF1398B92BDCAF0C6F62D23B1CDF52BEADEB001B522
