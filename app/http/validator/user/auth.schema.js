260ea922f397e2a7b51dbf4ee6485d267ae163a9
Amirhossein Abazari <92695669+am-abazari@users.noreply.github.com>
2022-05-10 18:30:39 +0430

    Update auth.schema.js

--- app/http/validator/user/auth.schema.js
+++ app/http/validator/user/auth.schema.js
@@ -6,5 +6,4 @@ const checkOTPSchema = Joi.object({
     mobile: Joi.string().trim().length(11).pattern(/^09[0-9]{9}$/mi).error(new Error("Mobile Number is Invalid")),
     code: Joi.string().trim().min(4).max(6).error(new Error("The Code is Invalid"))
 })
-
 module.exports = { getOTPSchema, checkOTPSchema }
